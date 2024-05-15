550ed9a6fff19205237f27592c961216cb24ee69
Adrian Pons <adrianpons21@gmail.com>
2024-05-15 18:45:36 +0200

    +Base de datos

---
+++ db/BaseDatos.sql
@@ -0,0 +1,70 @@
+CREATE DATABASE centralis;
+USE centralis;
+
+CREATE TABLE modulos (
+    CodigoModulo VARCHAR(10),
+    PRIMARY KEY (CodigoModulo)
+);
+
+CREATE TABLE celdas (
+    CodigoCelda VARCHAR(10),
+    Modulo VARCHAR(10),
+    PRIMARY KEY (CodigoCelda),
+    FOREIGN KEY (Modulo) REFERENCES modulos(CodigoModulo) ON DELETE SET NULL
+);
+
+CREATE TABLE turnos (
+    CodigoTurno INT,
+    HoraEntrada TIME,
+    HoraSalida TIME,
+    PRIMARY KEY (CodigoTurno)
+);
+
+CREATE TABLE presos (
+    NIF CHAR(9) NOT NULL,
+    Nombre CHAR(20) NOT NULL,
+    Apellidos CHAR(20),
+    Crimen INT,
+    Sexo CHAR(20),
+    Direccion CHAR(20),
+    CodigoPostal INT,
+    Correo CHAR(30),
+    Celda VARCHAR(10),
+    Telefono INT,
+    PRIMARY KEY (NIF),
+    FOREIGN KEY (Celda) REFERENCES celdas(CodigoCelda) ON DELETE SET NULL
+);
+
+CREATE TABLE empleado (
+    NIF CHAR(9) NOT NULL,
+    Nombre CHAR(20) NOT NULL,
+    Apellidos CHAR(20),
+    Puesto CHAR(20),
+    Contraseña VARCHAR(30),
+    Sexo CHAR(20),
+    Turno INT,
+    Direccion CHAR(20),
+    CodigoPostal INT,
+    Correo CHAR(30),
+    Celda VARCHAR(10),
+    Telefono INT,
+    PRIMARY KEY (NIF),
+    FOREIGN KEY (Celda) REFERENCES celdas(CodigoCelda) ON DELETE SET NULL,
+    FOREIGN KEY (Turno) REFERENCES turnos(CodigoTurno) ON DELETE SET NULL
+);
+
+CREATE TABLE puertas (
+    CodigoPuerta INT,
+    Abierta TINYINT,
+    PRIMARY KEY (CodigoPuerta)
+);
+
+CREATE TABLE registros (
+    CodigoRegistro INT,
+    Tipo CHAR(30),
+    Preso CHAR(9),
+    Motivo CHAR(30),
+    Fecha DATETIME,
+    PRIMARY KEY (CodigoRegistro),
+    FOREIGN KEY (Preso) REFERENCES presos(NIF) ON DELETE SET NULL
+);
