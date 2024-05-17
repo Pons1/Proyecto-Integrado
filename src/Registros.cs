f99ffff18a0e6cdf77dbb42949690f1b0e2efc74
DESKTOP-A2QVJH4\bigzz <magamu@floridauniversitaria.es>
2024-05-17 10:59:01 +0200

    inserciones codigos crimenes en db

---
+++ src/Registros.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROYECTO
+{
+    internal class Registros
+    {
+
+        int codigoRegistro;
+        string tipo;
+        string nifPreso;
+        string motivo;
+        DateTime fechaRegistro= DateTime.Now;
+
+
+        public Registros(string tipo, string nifPreso,string motivo, DateTime fechaRegistro)
+        {
+            this.tipo = tipo;
+            this.nifPreso = nifPreso;
+            this.motivo = motivo;
+            this.fechaRegistro = fechaRegistro;
+        }
+
+    }
+}
