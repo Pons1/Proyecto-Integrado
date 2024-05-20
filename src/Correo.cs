43bf452c640242db9e402703c514be3c8d6c52f2
DESKTOP-A2QVJH4\bigzz <magamu@floridauniversitaria.es>
2024-05-20 13:53:29 +0200

    Clase Correo Implementada en el proyecto

---
+++ src/Correo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+using System.Windows.Forms;
+
+namespace PROYECTO
+{
+    public class Correo
+    {
+        string mensaje;
+        string miCorreo = "manugamu@gmail.com";
+        string displayName = "Centralis";
+        string destinatario;
+        string asunto;
+
+        public string Mensaje { get => mensaje; set => mensaje = value; }
+        public string MiCorreo { get => miCorreo; set => miCorreo = value; }
+        public string DisplayName { get => displayName; set => displayName = value; }
+
+
+        public Correo(string destinatario, string asunto, string mensaje)
+        {
+            this.destinatario = destinatario;
+            this.asunto = asunto;
+            this.mensaje = mensaje;
+        }
+
+
+        public void EnviarCorreo()
+        {
+            MailAddress addressFrom = new MailAddress(this.MiCorreo, this.DisplayName);
+            MailAddress addressTo = new MailAddress(this.destinatario);
+            MailMessage message = new MailMessage(addressFrom, addressTo);
+
+            message.Subject = this.asunto;
+            message.IsBodyHtml = true;
+            message.Body = this.Mensaje;
+
+            SmtpClient cliente = new SmtpClient("smtp.gmail.com");
+            cliente.Port = 587;
+            cliente.EnableSsl = true;
+            cliente.UseDefaultCredentials = false;
+            cliente.Credentials = new NetworkCredential(this.MiCorreo, "vorz mhrb avqc htuk");
+
+            try
+            {
+                cliente.Send(message);
+                MessageBox.Show("Coreo enviado con exito");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
