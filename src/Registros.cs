e1855eba4b6791818b7a58bfe77099ed83473a02
DESKTOP-A2QVJH4\bigzz <magamu@floridauniversitaria.es>
2024-05-20 15:31:47 +0200

    Correos

--- src/Registros.cs
+++ src/Registros.cs
@@ -8,21 +8,5 @@ namespace PROYECTO
 {
     internal class Registros
     {
-
-        int codigoRegistro;
-        string tipo;
-        string nifPreso;
-        string motivo;
-        DateTime fechaRegistro= DateTime.Now;
-
-
-        public Registros(string tipo, string nifPreso,string motivo, DateTime fechaRegistro)
-        {
-            this.tipo = tipo;
-            this.nifPreso = nifPreso;
-            this.motivo = motivo;
-            this.fechaRegistro = fechaRegistro;
-        }
-
     }
 }
