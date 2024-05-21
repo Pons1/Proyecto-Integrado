991d37110dc15789427b4b5e3bcdaea73a9afd48
DESKTOP-A2QVJH4\bigzz <magamu@floridauniversitaria.es>
2024-05-21 12:45:13 +0200

    registros

--- src/Registros.cs
+++ src/Registros.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PROYECTO
 {
-    internal class Registros
+    public class Registros
     {
+        public static DataTable ObtenerRegistrosConPresos()
+        {
+            DataTable dt = new DataTable();
+
+            if (ConexionBD.Conexion != null)
+            {
+                try
+                {
+                    ConexionBD.AbrirConexion();
+
+                    string query = "SELECT CodigoRegistro, Tipo, PresoNIF, Motivo, Fecha FROM registros WHERE PresoNIF IS NOT NULL;";
+                    MySqlCommand cmd = new MySqlCommand(query, ConexionBD.Conexion);
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                    adapter.Fill(dt);
+
+                    ConexionBD.CerrarConexion();
+                }
+                catch (Exception ex)
+                {
+                    ConexionBD.CerrarConexion();
+                    throw new Exception("Error al obtener los registros: " + ex.Message);
+                }
+            }
+
+            return dt;
+        }
     }
 }
