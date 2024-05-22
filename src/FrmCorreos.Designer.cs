5e439ab005a97c4ffa55e9bdf8674107c9274ef7
DESKTOP-A2QVJH4\bigzz <magamu@floridauniversitaria.es>
2024-05-22 16:08:24 +0200

    ok

---
+++ src/FrmCorreos.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace PROYECTO
+{
+    partial class FrmCorreos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblAsunto = new System.Windows.Forms.Label();
+            this.lblPara = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(167, 111);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(516, 105);
+            this.textBox1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(167, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(516, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // lblAsunto
+            // 
+            this.lblAsunto.AutoSize = true;
+            this.lblAsunto.Location = new System.Drawing.Point(84, 43);
+            this.lblAsunto.Name = "lblAsunto";
+            this.lblAsunto.Size = new System.Drawing.Size(48, 16);
+            this.lblAsunto.TabIndex = 2;
+            this.lblAsunto.Text = "Asunto";
+            // 
+            // lblPara
+            // 
+            this.lblPara.AutoSize = true;
+            this.lblPara.Location = new System.Drawing.Point(84, 111);
+            this.lblPara.Name = "lblPara";
+            this.lblPara.Size = new System.Drawing.Size(36, 16);
+            this.lblPara.TabIndex = 3;
+            this.lblPara.Text = "Para";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(167, 266);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(516, 105);
+            this.textBox3.TabIndex = 4;
+            // 
+            // FrmCorreos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 541);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.lblPara);
+            this.Controls.Add(this.lblAsunto);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "FrmCorreos";
+            this.Text = "FrmCorreos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblAsunto;
+        private System.Windows.Forms.Label lblPara;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
